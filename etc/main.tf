0f248d566402e2b4810d6e1871da59df083a0e66
Matthew Todd <matthew@matthewtodd.org>
2022-12-16 20:36:40 -0500

    Manage the log group, so we can destroy it.

--- etc/main.tf
+++ etc/main.tf
@@ -37,4 +37,31 @@ resource "aws_lambda_function" "my_function" {
   source_code_hash = filebase64sha256("../src/my_function.zip")
 
   runtime = "nodejs14.x"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.my_function,
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "my_function" {
+  name              = "/aws/lambda/my_function"
+  retention_in_days = 3
+}
+
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    actions   = ["logs:CreateLogStream", "logs:putLogEvents"]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name   = "lambda_logging"
+  policy = data.aws_iam_policy_document.lambda_logging.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
 }
