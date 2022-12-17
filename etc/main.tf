4924fc5e9c27cb7c5e5fde5146dbe93ed3a2842b
Matthew Todd <matthew@matthewtodd.org>
2022-12-16 23:37:43 -0500

    Use stock lambda policy.

--- etc/main.tf
+++ etc/main.tf
@@ -58,17 +58,6 @@ data "aws_iam_policy_document" "my_policy" {
 
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = aws_iam_policy.lambda_logging.arn
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_policy" "lambda_logging" {
-  name   = "lambda_logging"
-  policy = data.aws_iam_policy_document.lambda_logging.json
-}
-
-data "aws_iam_policy_document" "lambda_logging" {
-  statement {
-    actions   = ["logs:CreateLogStream", "logs:putLogEvents"]
-    resources = ["arn:aws:logs:*:*:*"]
-  }
-}
