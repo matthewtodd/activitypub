b674f0247e6e329696d5175e56def2bd97008663
Matthew Todd <matthew@matthewtodd.org>
2022-12-20 21:26:35 -0500

    Put an API gateway in front of it!

--- etc/main.tf
+++ etc/main.tf
@@ -61,3 +61,41 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+resource "aws_apigatewayv2_api" "my_api" {
+  name          = "my_api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "my_api" {
+  api_id = aws_apigatewayv2_api.my_api.id
+
+  name        = "my_api_stage"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_integration" "my_function" {
+  api_id = aws_apigatewayv2_api.my_api.id
+
+  integration_uri    = aws_lambda_function.my_function.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "my_function" {
+  api_id = aws_apigatewayv2_api.my_api.id
+
+  route_key = "GET /my_function"
+  target    = "integrations/${aws_apigatewayv2_integration.my_function.id}"
+}
+
+resource "aws_lambda_permission" "my_api" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.my_api.execution_arn}/*/*"
+}
+
+output "base_url" {
+  value = aws_apigatewayv2_stage.my_api.invoke_url
+}
